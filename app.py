70fc813d2e3d8eddab737006d18038dbf32dd925
fatima-aamir <aamirfatima350@gmail.com>
2023-09-19 10:57:38 +0500

    add flask application app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+from calculator import Calculator  # Assuming your calculator logic is in a separate file
+
+app = Flask(__name__)
+
+# Initialize your calculator
+calculator = Calculator()
+
+@app.route('/')
+def index():
+    return "Welcome to the Calculator App"
+
+# Define routes for your calculator functions
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = calculator.add(num1, num2)
+    return jsonify({"result": result})
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = calculator.subtract(num1, num2)
+    return jsonify({"result": result})
+
+# Add more routes for other calculator functions as needed
+
+if __name__ == '__main__':
+    app.run(debug=True)
